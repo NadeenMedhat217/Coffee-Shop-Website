2fc3c1f4984b02154fb949a9f83edd5db4472c10
nadeen-kirolos <nadeen.kirolos@vodafone.com>
2024-09-18 17:25:13 +0300

    Destructuring for the props

--- coffee-shop/src/genericComponents/Button/Button.js
+++ coffee-shop/src/genericComponents/Button/Button.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "../Button/Button.module.css";
 
-const Button = (props) => {
+const Button = ({ label, handleClick }) => {
   return (
     <div className={styles.container}>
-      <button className={styles.button}>{props.text}</button>
+      <button onClick={handleClick} className={styles.button}>
+        {label.text}
+      </button>
     </div>
   );
 };
