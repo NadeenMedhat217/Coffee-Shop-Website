b659ee70cdc64f4bfeab8cb34b2a179c7532fb05
nadeen-kirolos <nadeen.kirolos@vodafone.com>
2024-09-18 17:06:18 +0300

    Reusable Grey Horizontal Line

--- coffee-shop/src/pages/Order/Order.jsx
+++ coffee-shop/src/pages/Order/Order.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CustomizedRadioButton from "./components/CustomizedRadioButton/CustomizedRadioButton";
+import GreyHorizontalLine from "../../genericComponents/GreyHorizontalLine";
 
 const Order = () => {
   return (
     <div>
       Order
       <CustomizedRadioButton />
+      <GreyHorizontalLine/>
     </div>
   );
 };
