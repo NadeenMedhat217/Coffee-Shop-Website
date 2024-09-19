8d97340e3a1ddbce2ee146117a55a018b74c1801
nadeen-kirolos <nadeen.kirolos@vodafone.com>
2024-09-19 17:46:08 +0300

    Solving conflicts with main

--- coffee-shop/src/pages/Order/Order.jsx
+++ coffee-shop/src/pages/Order/Order.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import Header from "../../genericComponents/Header/Header";
 import Button from "../../genericComponents/Button/";
 import CustomizedRadioButton from "./components/CustomizedRadioButton";
+import CustomizedTextButton from "./components/CustomizedTextButton";
+import styles from "./components/CustomizedTextButton/CustomizedTextButton.module.css";
 import DeliveryAddress from "./components/DeliveryAddress";
 import GreyHorizontalLine from "../../genericComponents/GreyHorizontalLine";
 import LightOrangeLine from "./components/LightOrangeLine";
@@ -10,7 +13,12 @@ const Order = () => {
   return (
     <div>
       Order page
+      <Header />
       <CustomizedRadioButton />
+      <DeliveryAddress
+        location="Jl. Kpg Sutoyo"
+        addressDetails="Kpg. Sutoyo No. 620, Bilzen, Tanjungbalai."
+      />
       <div className={styles.buttonContainer}>
         <div className={styles.buttonWrapper}>
           <CustomizedTextButton icon={icons.edit} text="Edit Address" />
