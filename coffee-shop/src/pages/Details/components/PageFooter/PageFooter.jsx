c520890c540056ebb378c0f46235bfc1c3678780
Youssef Ayman <103893221+youssefayman22@users.noreply.github.com>
2024-09-22 16:54:12 +0300

    Fixing issues

--- coffee-shop/src/pages/Details/components/PageFooter/PageFooter.jsx
+++ coffee-shop/src/pages/Details/components/PageFooter/PageFooter.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../PageFooter/PageFooter.module.css";
 import Price from "./Price/Price";
 import Button from "../../../../genericComponents/Button/Button";
 
 const PageFooter = ({ itemPrice }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/order");
+  };
   return (
     <div className={styles.Container}>
       <Price itemPrice={itemPrice} />
       <div className={styles.buttonContainer}>
-        <Button label="Buy Now" />
+        <Button label="Buy Now" handleClick={handleClick}/>
       </div>
     </div>
   );
