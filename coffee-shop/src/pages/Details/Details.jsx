9df603f7088204f5b60b0baeb51ddae172a6166c
youssef-hamdy <youssef.hamdy@vodafone.com>
2024-09-23 15:16:50 +0300

    Details Page and css fixes

--- coffee-shop/src/pages/Details/Details.jsx
+++ coffee-shop/src/pages/Details/Details.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import Header from "../../genericComponents/Header/Header";
+import DetailsHeroImage from "./components/DetailsHeroImage";
+import images from "../../assets/images/imageImports";
+import GreyHorizontalLine from "../../genericComponents/GreyHorizontalLine";
+import ProductDescription from "./components/ProductDescription";
+import DescriptionBox from "./components/DescriptionBox";
+import SizeBox from "./components/SizeBox";
+import PageFooter from "./components/PageFooter";
 const Details = () => {
-  return <div>Details</div>;
+  return (
+    <>
+      <Header pageName="Details" />
+      <DetailsHeroImage imageName={images.coffee7} />
+      <ProductDescription itemName="Coffee Mocha" />
+      <GreyHorizontalLine />
+      <DescriptionBox textDiscription="A cappuccino is an approximately 150 ml (5 oz) beverage, with 25 ml of espresso coffee and 85ml of fresh milk the fo.." />
+      <SizeBox/>
+      <PageFooter itemPrice= "5.06"/>
+    </>
+  );
 };
 
 export default Details;
---
+++ coffee-shop/src/pages/Details/components/DetailsHeroImage/index.js
@@ -0,0 +1,2 @@
+import {default as DetailsHeroImage} from './DetailsHeroImage'
+export default DetailsHeroImage
