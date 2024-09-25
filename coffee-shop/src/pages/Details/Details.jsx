469cd9e12355a3d82ffd5788d78c9d2cc908b50a
youssef-hamdy <youssef.hamdy@vodafone.com>
2024-09-25 13:23:46 +0300

    Fetching data in details page from JSON file

--- coffee-shop/src/pages/Details/Details.jsx
+++ coffee-shop/src/pages/Details/Details.jsx
@@ -1,22 +1,32 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import Header from "../../genericComponents/Header/Header";
 import DetailsHeroImage from "./components/DetailsHeroImage";
-import images from "../../assets/images/imageImports";
 import GreyHorizontalLine from "../../genericComponents/GreyHorizontalLine";
 import ProductDescription from "./components/ProductDescription";
 import DescriptionBox from "./components/DescriptionBox";
 import SizeBox from "./components/SizeBox";
 import PageFooter from "./components/PageFooter";
+import DetailsFunction from "../../utilities/DetailsFunction";
+
 const Details = () => {
+  // will get the category and id form HandleCardClick function in the home page
+  // Get the category and id from the URL params
+  // useParams() is a hook that is used to extract category and id from the URL.
+  const { category, id } = useParams();
+
+  // Fetch the coffee data from the JSON using DetailsFunction
+  const coffeeItem = DetailsFunction(id, category);
+  const imagePath = require(`../../assets/images/${coffeeItem.image}`);
   return (
     <>
       <Header pageName="Details" />
-      <DetailsHeroImage imageName={images.coffee7} />
-      <ProductDescription itemName="Coffee Mocha" />
+      <DetailsHeroImage imageName={imagePath} />
+      <ProductDescription itemName={coffeeItem.name} />
       <GreyHorizontalLine />
-      <DescriptionBox textDiscription="A cappuccino is an approximately 150 ml (5 oz) beverage, with 25 ml of espresso coffee and 85ml of fresh milk the fo.." />
-      <SizeBox/>
-      <PageFooter itemPrice= "5.06"/>
+      <DescriptionBox textDiscription={coffeeItem.description} />
+      <SizeBox />
+      <PageFooter itemPrice={coffeeItem.price} />
     </>
   );
 };
