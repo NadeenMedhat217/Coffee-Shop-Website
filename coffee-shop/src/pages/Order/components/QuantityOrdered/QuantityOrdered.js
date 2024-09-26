d5dbd4ff55cfb3e6587a35ec32ffadfbec36b892
nadeen-kirolos <nadeen.kirolos@vodafone.com>
2024-09-26 13:27:54 +0300

    Update the Store with quantity value

--- coffee-shop/src/pages/Order/components/QuantityOrdered/QuantityOrdered.js
+++ coffee-shop/src/pages/Order/components/QuantityOrdered/QuantityOrdered.js
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  updateItemQuantity,
+  itemsSelector,
+} from "../../../../store/itemsSlice";
 import styles from "./QuantityOrdered.module.css";
 import QuantityButton from "../QuantityButton";
 import icons from "../../../../assets/icons/iconImport";
 
-const QuantityOrdered = ({ image, name, onQuantityChange }) => {
-  const [quantity, setQuantity] = useState(1);
+const QuantityOrdered = ({ image, name }) => {
+  const dispatch = useDispatch();
+  const items = useSelector(itemsSelector);
+  const item = items.find((item) => item.name === name);
+
+  const [quantity, setQuantity] = useState(item ? item.quantity : 1);
 
   const increaseQuantity = () => setQuantity(quantity + 1);
   const decreaseQuantity = () => setQuantity(quantity > 1 ? quantity - 1 : 1);
 
   useEffect(() => {
-    onQuantityChange(quantity);
-  }, [quantity, onQuantityChange]);
+    if (item) {
+      dispatch(updateItemQuantity({ id: item.id, quantity }));
+    }
+  }, [quantity, dispatch, item]);
 
+  //console.log(quantity);
   return (
     <div className={styles.container}>
       <div className={styles.orderedItem}>
